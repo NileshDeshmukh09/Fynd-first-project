b7a2d30b29bed73ccd9954114ebf37bb7ae2d1f3
NileshDeshmukh09 <nilesdeshmukh0908@gmail.com>
2022-06-26 21:44:46 +0530

    node modules be gone

--- public/javaScript/nav.js
+++ public/javaScript/nav.js
@@ -1,5 +1,12 @@
 const menuButton = document.getElementById( 'menu-button' );
 
 menuButton.addEventListener( 'click', function() {
-    alert( 'Menu button was clicked' );
+    // you get a list of elements
+    const navItems = document.querySelectorAll( '.nav-items' );
+
+    // go through the list and add / remove d-none
+    for( let i = 0; i < navItems.length; i++ ) {
+        // add d-none if not present / remove d-none if present
+        navItems[i].classList.toggle( 'd-sm-none' );
+    }
 });
